6ebda8375c7817de93cdb996588fccec2230c638
HamzaArif538 <sardarhamza538@gmail.com>
2023-03-06 22:07:48 +0500

    Front page is being ready

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from "./My_Components/Header";
+import Todos from "./My_Components/Todos";
+import Footer from "./My_Components/Footer";
 
 function App() {
+const onDelete = (todo) => {
+  console.log("Todo has been deleted!", todo);
+}
+
+
+  let todos= [
+    {
+      sno:1,
+      title: "Go to the market",
+      desc: "You need to go to the Market to get this job done"
+    },
+    
+    {
+      sno:2,
+      title: "Go to the mall",
+      desc: "You need to go to the Mall to get this job done"
+    },
+    
+    {
+      sno:3,
+      title: "Go to the university",
+      desc: "You need to go to the University to get this job done"
+    }
+  ]
+
+
   return (
     <>
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Todos-List</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" >Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">About</a>
-        </li>
-        
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-    
+      <Header title="My Todos List" searchBar={false} />
+      <Todos todos={todos} onDelete={onDelete} />
+      <Footer/>
     </>
   );
 }
